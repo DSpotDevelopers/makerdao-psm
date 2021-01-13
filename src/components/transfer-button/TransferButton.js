1ef1c2791b7fc03cc320469ef2aa6c27ed2be56b
Maykel Lopez <maykel.meneses89@gmail.com>
2021-01-13 14:00:15 +0100

    Added states to transfer button
    Missing check asset

--- src/components/transfer-button/TransferButton.js
+++ src/components/transfer-button/TransferButton.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./TransferButton.scss";
 import arrowUp from "../../assets/arrow-up.svg";
 
 const TransferButton = () => {
-    return <button className={`TransferButton`}><img src={arrowUp} className="img img1" alt={'arrow'}/> <img src={arrowUp} className="img img2" alt={'arrow'}/> </button>;
+    const [clicked, setClicked] = useState(false);
+    const [fulfilled, setFulfilled] = useState(false);
+
+    const handleClick = () => {
+      if (!clicked) setClicked(true);
+      if (clicked) setFulfilled(true);
+
+      if (clicked && fulfilled) {
+          setClicked(false);
+          setFulfilled(false);
+      }
+    };
+
+    return <button className={`TransferButton ${fulfilled ? 'Fulfilled' : ''} ${clicked ? 'Clicked' : ''}`} onClick={handleClick}>
+        {!fulfilled && <>
+            <img src={arrowUp} className="img img1" alt={'arrow'}/>
+            <img src={arrowUp} className="img img2" alt={'arrow'}/>
+        </>}
+    </button>;
 };
 
 export default TransferButton;
