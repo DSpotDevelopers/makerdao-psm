7dd33a48a0c0ae2ae94cea70571515ee9372565d
Maykel Lopez <maykel.meneses89@gmail.com>
2021-01-14 12:21:30 +0100

    Finished connect button layout

--- src/components/connect-button/ConnectButton.jsx
+++ src/components/connect-button/ConnectButton.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ConnectButton.scss';
 import imgWallet from '../../assets/wallet.svg';
 
-const ConnectButton = () => (
-  <div className="ConnectButton">
-    <img src={imgWallet} alt="wallet" className="img" />
-    Connect to Wallet...
-  </div>
-);
+const ConnectButton = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [connecting, setConnecting] = useState(false);
+
+  return (
+    <div className="ConnectButtonWrapper no-select">
+      {/* eslint-disable-next-line max-len */}
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+      <div className={`Gradient ${connecting ? 'Connecting' : ''}`} onClick={() => setConnecting(!connecting)}>
+        <img src={imgWallet} alt="wallet" className="img" />
+      </div>
+      {connecting && <span>Connect to Wallet...</span>}
+      {!connecting && <input type="text" placeholder="Enter wallet id" />}
+    </div>
+  );
+};
 
 export default ConnectButton;
