d76f88377e91eb9740826229a37d64ec77661515
Maykel Lopez <maykel.meneses89@gmail.com>
2021-01-15 10:48:49 +0100

    Adding api connection to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from './views/main/Main';
+import { usePsmService } from './services/psm/PsmProvider';
 
 function App() {
+  const psmService = usePsmService();
+  // eslint-disable-next-line no-unused-vars
+  const [stats, setStats] = useState(undefined);
+  // eslint-disable-next-line no-unused-vars
+  const [fees, setFees] = useState(undefined);
+  // const [ilk, setIlk] = useState();
+  useEffect(async () => {
+    setStats(await psmService.getStats('USDC'));
+    setFees(await psmService.getFees());
+  }, []);
+
   return (
     <Main />
   );
